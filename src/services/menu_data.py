1a417fb1f6aa1a831e66d4162564325c9f296541
willianmenezess <willmenezes@outlook.com.br>
2023-10-06 17:13:39 -0300

    feat: resolve requisito 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,35 @@
+# from typing import Set
+from models.dish import Dish
+from models.ingredient import Ingredient
+import csv
+
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        with open(source_path) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                print('Linha CSV: ', row)
+                instance_dish = Dish(row["dish"], float(row["price"]))
+                if instance_dish not in self.dishes:
+                    instance_dish.add_ingredient_dependency(
+                        Ingredient(row["ingredient"]),
+                        int(row["recipe_amount"]),
+                    )
+                    self.dishes.add(instance_dish)
+                else:
+                    for dish in self.dishes:
+                        if dish == instance_dish:
+                            dish.add_ingredient_dependency(
+                                Ingredient(row["ingredient"]),
+                                int(row["recipe_amount"]),
+                            )
+
+
+if __name__ == "__main__":
+    menu_data = MenuData("tests/mocks/menu_base_data.csv")
+    print(menu_data)
+    print('Pratos do menu:', menu_data.dishes)
+    # print(menu_data.dishes[0])
