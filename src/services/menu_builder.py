0250e9ffacb074be864910fc1c65c50d1d94e642
willianmenezess <willmenezes@outlook.com.br>
2023-10-07 19:07:25 -0300

    feat: resolve requisito 6

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -1,6 +1,6 @@
 from typing import Dict, List
 
-# from services.inventory_control import InventoryMapping
+from services.inventory_control import InventoryMapping
 from services.menu_data import MenuData
 
 DATA_PATH = "data/menu_base_data.csv"
@@ -10,19 +10,19 @@ INVENTORY_PATH = "data/inventory_base_data.csv"
 class MenuBuilder:
     def __init__(self, data_path=DATA_PATH, inventory_path=INVENTORY_PATH):
         self.menu_data = MenuData(data_path)
-        # self.inventory = InventoryMapping(inventory_path)
+        self.inventory = InventoryMapping(inventory_path)
 
-    # def make_order(self, dish_name: str) -> None:
-    #     try:
-    #         curr_dish = [
-    #             dish
-    #             for dish in self.menu_data.dishes
-    #             if dish.name == dish_name
-    #         ][0]
-    #     except IndexError:
-    #         raise ValueError("Dish does not exist")
+    def make_order(self, dish_name: str) -> None:
+        try:
+            curr_dish = [
+                dish
+                for dish in self.menu_data.dishes
+                if dish.name == dish_name
+            ][0]
+        except IndexError:
+            raise ValueError("Dish does not exist")
 
-    #     self.inventory.consume_recipe(curr_dish.recipe)
+        self.inventory.consume_recipe(curr_dish.recipe)
 
     # Req 4
     def get_main_menu(self, restriction=None) -> List[Dict]:
@@ -43,7 +43,8 @@ class MenuBuilder:
                 "price": filt_dish.price,
                 "restrictions": filt_dish.get_restrictions(),
             }
-            list_dish_dict.append(dish_dict)
+            if self.inventory.check_recipe_availability(filt_dish.recipe):
+                list_dish_dict.append(dish_dict)
 
         return list_dish_dict
 
